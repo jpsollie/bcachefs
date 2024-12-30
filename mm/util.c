fdafd17b017b5f8bd6d44c3bc5d74a91ea214795
jpsollie <janpieter.sollie@edpnet.be>
2024-12-30 10:05:53 +0100

    fixup: allow UINT_MAX size rhashtables
    bcachefs fsck sometimes requires huge rhashtables.
    While this code can handle it,
    there are kworkers for rhashtable maintenance.
    Those seem to copy back-and-forth,
    and try to allocate a new memory block as large as the rhashtable.
    This causes problems:
    kvmalloc_noprof doesn't allow huge memory blocks.
    raising the limit to UINT_MAX instead of INT_MAX fixes the issue.
    
    Signed-off-by: Janpieter Sollie <janpieter.sollie@edpnet.be>

--- mm/util.c
+++ mm/util.c
@@ -658,7 +658,7 @@ void *__kvmalloc_node_noprof(DECL_BUCKET_PARAMS(size, b), gfp_t flags, int node)
 		return NULL;
 
 	/* Don't even allow crazy sizes */
-	if (unlikely(size > INT_MAX)) {
+	if (unlikely(size > UINT_MAX)) {
 		WARN_ON_ONCE(!(flags & __GFP_NOWARN));
 		return NULL;
 	}
